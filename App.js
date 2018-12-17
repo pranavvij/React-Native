dd4f52b71837480efb73e68a5d4347a368ff65b8
pranav vij <vij.pranav2@gmail.com>
2018-12-17 15:23:07 -0500

    flex layout

--- App.js
+++ App.js
@@ -2,31 +2,40 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Image, View, Text } from 'react-native';
 
-const styles = StyleSheet.create({
-    bigblue: {
-      color: 'blue',
-      fontWeight: 'bold',
-      fontSize: 30
-    },
-    red: {
-      color: 'red'
-    }
-});
+// const styles = StyleSheet.create({
+//     bigblue: {
+//       color: 'blue',
+//       fontWeight: 'bold',
+//       fontSize: 30
+//     },
+//     red: {
+//       color: 'red'
+//     }
+// });
+//
+// export default class Bananas extends Component {
+//   render(){
+//     let pic = {
+//         uri: "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"
+//     }
+//     return (<View>
+//                 <Text style={styles.red}>Just Red</Text>
+//                 <Text style={styles.bigblue}>Just Blue</Text>
+//                 <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+//                 <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+//             </View>);
+//   }
+// }
 
-export default class Bananas extends Component {
-  render(){
-    let pic = {
-        uri: "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"
+export default class FixedDimensionsBasics extends Component {
+    render(){
+      return(<View style={{flex:1,height:50}}>
+                <View style={{flex:1, backgroundColor: 'powderblue'}}/>
+                <View style={{flex:2, backgroundColor: 'skyblue'}}/>
+                <View style={{flex:3, backgroundColor: 'steelblue'}}/>
+              </View>);
     }
-    return (<View>
-                <Text style={styles.red}>Just Red</Text>
-                <Text style={styles.bigblue}>Just Blue</Text>
-                <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
-                <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
-            </View>);
-  }
 }
 
 
-
-AppRegistry.registerComponent('ReactTutorial', () => Bananas);
+AppRegistry.registerComponent('ReactTutorial', () => FixedDimensionsBasics);
