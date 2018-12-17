bd597f4ef3b422dd029ba54ec5038b6c7cb0a988
pranav vij <vij.pranav2@gmail.com>
2018-12-17 15:54:15 -0500

    Button  done

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Image, View, Text, TextInput } from 'react-native';
+import { AppRegistry, StyleSheet, Image, View, Text, TextInput, Button,Alert } from 'react-native';
 
 // const styles = StyleSheet.create({
 //     bigblue: {
@@ -37,25 +37,34 @@ import { AppRegistry, StyleSheet, Image, View, Text, TextInput } from 'react-nat
 //     }
 // }
 
-export default class PizzaTranslator extends Component{
-  constructor(props){
-    super(props);
-    this.state = {text: ''};
-  }
+// export default class PizzaTranslator extends Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {text: ''};
+//   }
+//
+//   render(){
+//     return (
+//         <View style={{padding: 10}}>
+//             <TextInput style={{height: 40}}
+//             placeholder="Type here to translate!!"
+//             onChangeText={(text) => this.setState({text})}/>
+//             <Text style={{padding: 10, fontSize: 42}}>
+//                 {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+//             </Text>
+//         </View>
+//       )
+//   }
+// }
 
+
+export default class ButtonPressComponent extends Component{
   render(){
     return (
-        <View style={{padding: 10}}>
-            <TextInput style={{height: 40}}
-            placeholder="Type here to translate!!"
-            onChangeText={(text) => this.setState({text})}/>
-            <Text style={{padding: 10, fontSize: 42}}>
-                {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-            </Text>
-        </View>
-      )
+        <Button title="Press Me" onPress={() => {Alert.alert('You tapped the button!');}}/>
+      );
   }
 }
 
 
-AppRegistry.registerComponent('ReactTutorial', () => PizzaTranslator);
+AppRegistry.registerComponent('ReactTutorial', () => ButtonPressComponent);
