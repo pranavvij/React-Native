9fd5990ad24ce0fb1f7bc67b80526148020ea275
pranav vij <vij.pranav2@gmail.com>
2018-12-17 16:04:31 -0500

    Button css done

--- App.js
+++ App.js
@@ -58,13 +58,65 @@ import { AppRegistry, StyleSheet, Image, View, Text, TextInput, Button,Alert } f
 // }
 
 
-export default class ButtonPressComponent extends Component{
+// export default class ButtonPressComponent extends Component{
+//   render(){
+//     return (
+//         <Button title="Press Me" onPress={() => {Alert.alert('You tapped the button!');}}/>
+//       );
+//   }
+// }
+
+export default class ButtonBasics extends Component {
+  _onPressButton(){
+    Alert.alert('You tapped the button!');
+  }
+
   render(){
     return (
-        <Button title="Press Me" onPress={() => {Alert.alert('You tapped the button!');}}/>
+      <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button
+          onPress={this._onPressButton}
+          title="Press Me"
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          onPress={this._onPressButton}
+          title="Press Me"
+          color="#841584"
+        />
+      </View>
+      <View style={styles.alternativeLayoutButtonContainer}>
+        <Button
+          onPress={this._onPressButton}
+          title="This looks great!"
+        />
+        <Button
+          onPress={this._onPressButton}
+          title="OK!"
+          color="#841584"
+        />
+      </View>
+    </View>
       );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   backgroundColor: '#000000',
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
 
 AppRegistry.registerComponent('ReactTutorial', () => ButtonPressComponent);
